52761e393f639a6b6efb7e9b79bc33fae841bee5
Munawar Hussain Shakil <shakil.ruet.12@gmail.com>
2019-06-21 23:50:54 +0600

    upload build

---
+++ precache-manifest.02044224dd5dde88ad7241b55ed6ef73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50193c28619330a9d7b93ccd0e1f422c",
+    "url": "/mini-checkers-build/index.html"
+  },
+  {
+    "revision": "b5ba63fd41186233758c",
+    "url": "/mini-checkers-build/static/css/main.9eee456b.chunk.css"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/mini-checkers-build/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "b5ba63fd41186233758c",
+    "url": "/mini-checkers-build/static/js/main.52744f55.chunk.js"
+  },
+  {
+    "revision": "d191857c70b8f25b9a22",
+    "url": "/mini-checkers-build/static/js/runtime~main.1763b151.js"
+  }
+]);
